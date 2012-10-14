41f609fe5f3b8f24feb9d302464ee3e6ab99f187
Patrick Forringer <patrick@forringer.com>
2012-10-14 13:12:40 -0500

    white space removal

--- wymi/static/app/main.js
+++ wymi/static/app/main.js
@@ -7,9 +7,7 @@ require([
   
   // Bootstrap Framework JS
   "bootstrap"
-],
-
-function(app, Router) {
+],function(app, Router) {
 
   // Define your master router on the application namespace and trigger all
   // navigation from this instance.
